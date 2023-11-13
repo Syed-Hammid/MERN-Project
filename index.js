ae19de55fb3947c24a063ae9942fcb0bce488be9
Syed-Hammid <iamsyedhammid@gmail.com>
2023-11-13 16:37:52 +0530

    cors added

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 // import { Book } from './models/bookmodel.js';
 import router from './routes/bookRoute.js';
+import { cors } from 'cors';
 const app = express();
 const mongoDBURL = "mongodb+srv://syed:12345@cluster0.6xbu7pu.mongodb.net/?retryWrites=true&w=majority";
 app.use(express.json());
+app.use(cors());
 app.get("/", (req,res) => {
     res.status(201).send("Express Connected");
     // console.log("Backend is Connected");
